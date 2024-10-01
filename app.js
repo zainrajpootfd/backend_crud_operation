2234ddb0d7fc347f9fe12cb73a4c52a0492037d5
zain <zainrajpootfd@gmail.com>
2024-10-01 22:00:51 +0500

    add crud feature with ejs and user model

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const userModel = require("./models/user");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/read", async function (req, res) {
+  let users = await userModel.find({});
+  res.render("read", { users });
+});
+
+app.post("/create", async function (req, res) {
+  let { name, email, image } = req.body;
+  let createdUser = await userModel.create({
+    name,
+    email,
+    image,
+  });
+  res.send(createdUser);
+});
+
+app.get("/delete/:id", async function (req, res) {
+  let users = await userModel.findOneAndDelete({ _id: req.params.id });
+  res.redirect("/read");
+});
+
+app.get("/edit/:userid", async function (req, res) {
+  let user = await userModel.findOne({ _id: req.params.userid });
+  res.render("edit", { user });
+});
+
+app.post("/update/:userid", async function (req, res) {
+  let { name, email, image } = req.body;
+  let user = await userModel.findOneAndUpdate(
+    { _id: req.params.userid },
+    { name, email, image },
+    { new: true }
+  );
+  res.redirect("/read");
+});
+app.listen(5173);
